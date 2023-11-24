445a6bbb2c11620c637d7d50d5d26bbb651a4661
null <null>
2023-11-24 12:41:12 -0600

    Add Spotify Icon to FormTopTen and FormAgregar

--- FormAgregar.Designer.cs
+++ FormAgregar.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAgregar));
             this.lbTitulo = new System.Windows.Forms.Label();
             this.txtbTitulo = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -109,6 +110,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lbTitulo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormAgregar";
             this.Text = "Agregar Música";
             this.ResumeLayout(false);
